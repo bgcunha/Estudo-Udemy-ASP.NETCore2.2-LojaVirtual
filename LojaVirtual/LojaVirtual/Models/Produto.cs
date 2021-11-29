91adca7a936e051a2c7b2eb390a3aa7193f92a9b
Bruno Gomes <brunnu.cunha@gmail.com>
2021-11-29 12:12:26 -0300

    Validações no produto

--- LojaVirtual/LojaVirtual/Models/Produto.cs
+++ LojaVirtual/LojaVirtual/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using LojaVirtual.Controllers.Libraries.Lang;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,21 +8,37 @@ namespace LojaVirtual.Models
     public class Produto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         [Display(Name = "Preço")] 
         public decimal Valor { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [Range(0, 1000000, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
         public int Quantidade { get; set; }
 
-        //Frete
+        //Frete Correio
+        [Range(0.001, 30, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
         public double Peso { get; set; }
-        public int largura { get; set; }
+
+        [Range(11, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
+        public int Largura { get; set; }
+
+        [Range(2, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
         public int Altura { get; set; }
-        public int Profundidade { get; set; }
+
+        [Range(16, 105, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E006")]
+        public int Comprimento { get; set; }
 
         //Banco de dados - Relacionamento entre tabelas
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
         [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
 
